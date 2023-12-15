3aa6969190a930d6afbd6a91407b11bf8b3b2461
Tyler Jones <tyljont@amazon.com>
2023-12-15 12:39:40 -0700

    adding nanoid function to stress CPU

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from flask import Flask
+from nanoid import generate
 
 app = Flask(__name__)
 
@@ -13,3 +14,9 @@ app.config.update({"DEBUG": bool(flask_debug)})
 def index():
     simple_date = datetime.now()
     return "Hello, World from PyPy 3, Gunicorn and Gevent! {}".format(simple_date.strftime("%Y-%m-%d %H:%M:%S.%f"))
+
+
+@app.route("/getID")
+def getID():
+    id = generate(size=10)
+    return "nanoid: {id}".format(id = id)
