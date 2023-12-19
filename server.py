caa95084ed0ef5c758471020ae7033dbde0a7af8
Tyler Jones <tyljont@amazon.com>
2023-12-19 10:41:03 -0700

    adding dynamodb logic to make sure gevent has a reason to work

--- server.py
+++ server.py
@@ -2,13 +2,33 @@ import os
 from datetime import datetime
 from flask import Flask
 from nanoid import generate
+import boto3
 
 app = Flask(__name__)
 
 flask_debug = os.environ.get("FLASK_DEBUG", False)
 
 app.config.update({"DEBUG": bool(flask_debug)})
+dynamodb_table_name="python_url_shortener"
 
+def get_id():
+    id = generate(size=10)
+    return id
+
+def put_url_id_in_dynamo(url_id, dynamodb_table_name):
+    #TODO: move cliant initialization out to main function
+    dynamodb = boto3.resource('dynamodb')
+
+    url_table = dynamodb.Table(dynamodb_table_name)
+
+    response = url_table.put_item(
+        Item={
+            "url_id" : url_id,
+            "url": "https://aws.amazon.com/ec2/graviton/"
+        }
+    )
+    print("stored item in dynamo")
+    return response
 
 @app.route("/")
 def index():
@@ -16,7 +36,8 @@ def index():
     return "Hello, World from PyPy 3, Gunicorn and Gevent! {}".format(simple_date.strftime("%Y-%m-%d %H:%M:%S.%f"))
 
 
-@app.route("/getID")
-def getID():
-    id = generate(size=10)
-    return "nanoid: {id}".format(id = id)
+@app.route("/shorten_url")
+def shorten_url():
+    url_id = get_id()
+    return put_url_id_in_dynamo(url_id, dynamodb_table_name)
+    
