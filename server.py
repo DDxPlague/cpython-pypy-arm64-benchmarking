3a8bcac6ef8892beda43ec42df19c5c8335e5073
Tyler Jones <tyljont@amazon.com>
2024-01-02 13:58:39 -0700

    adding pycryptodome for repro

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import os
 from datetime import datetime
 from flask import Flask
 from nanoid import generate
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
 import boto3
 
 app = Flask(__name__)
@@ -19,7 +21,6 @@ def get_id():
     return id
 
 def put_url_id_in_dynamo(url_id, dynamodb_table_name, dynamo_db_client):
-    #TODO: move cliant initialization out to main function
 
     url_table = dynamo_db_client.Table(dynamodb_table_name)
 
@@ -29,7 +30,7 @@ def put_url_id_in_dynamo(url_id, dynamodb_table_name, dynamo_db_client):
             "url": "https://aws.amazon.com/ec2/graviton/"
         }
     )
-    print("stored item in dynamo")
+    print(response)
     return response
 
 @app.route("/")
@@ -43,3 +44,23 @@ def shorten_url():
     url_id = get_id()
     return put_url_id_in_dynamo(url_id, dynamodb_table_name, dynamo_db_client)
     
+
+@app.route("/test_aes_encrypt")
+def test_aes_encrypt():
+    key = get_random_bytes(32)
+    data_to_encrypt = "tyljont@amazon.com"
+    data = data_to_encrypt.encode('utf-8')
+    cipher_encrypt = AES.new(key, AES.MODE_CFB)
+    ciphered_bytes = cipher_encrypt.encrypt(data)
+
+    iv = cipher_encrypt.iv
+    # ciphered_data = ciphered_bytes
+
+    cipher_decrypt = AES.new(key, AES.MODE_CFB, iv=iv)
+    deciphered_bytes = cipher_decrypt.decrypt(ciphered_bytes)
+    decrypted_data = deciphered_bytes.decode('utf-8')
+
+    # assert data_to_encrypt == decrypted_data, 'Original data does not match the result'
+    result = ("original data: " + str(data_to_encrypt) + " | encrypted data: " + str(ciphered_bytes) + " | decrypted data: " + str(decrypted_data))
+
+    return (result)
